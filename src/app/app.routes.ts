480b342b3d307b102e5fe3bdf235c4d716358cdc
amadiaze jaafar <Jaafar.Amadiaze@emsi-edu.ma>
2025-08-11 15:14:17 +0100

    home test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { Routes } from '@angular/router';
+import { GraphsComponent } from './components/graphs/graphs.component';
+import { HomeComponent } from './components/home/home.component';
+   ;
 
+export const routes: Routes = [
+    {path:'' , component: HomeComponent},
+    { path: 'graphs', component: GraphsComponent },
+    
 
-export const routes: Routes = [];
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })

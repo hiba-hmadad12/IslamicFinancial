d48ccf6b7a28957af748f0847f27109e0a477eee
amadiaze jaafar <Jaafar.Amadiaze@emsi-edu.ma>
2025-08-15 17:52:23 +0100

    graphs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { GraphsComponent } from './components/graphs/graphs.component';
 import { HomeComponent } from './components/home/home.component';
-   ;
+import { OrganismeDetailsComponent } from './components/organisme-details/organisme-details.component';
+   
 
 export const routes: Routes = [
     {path:'' , component: HomeComponent},
     { path: 'graphs', component: GraphsComponent },
+    { path: 'organisme-details/:id', component: OrganismeDetailsComponent },
     
 
 ];
--- src/app/components/graphs/graphs.component.ts
+++ src/app/components/graphs/graphs.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
   styleUrls: ['./graphs.component.css']
 })
-export class GraphsComponent {}
+export class GraphsComponent { constructor(private router: Router) {}
+
+  goToDetails(company: { id: number; name: string; sector: string; country: string }) {
+    this.router.navigate(['/organisme-details', company.id], { state: { company } });
+  }}

8020f562b93a6aa715a48a47e217a4e881de66d0
hiba hmadad <hibahmadad12@gmail.com>
2025-09-04 18:31:54 +0100

    Initial commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
   { path: 'about', loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent) },
   { path: 'organisme-details/:id', loadComponent: () => import('./components/organisme-details/organisme-details.component').then(m => m.OrganismeDetailsComponent), canActivate: [authGuard] },
+  { path: 'contact', loadComponent: () => import('./components/contact/contact.component').then(m => m.ContactComponent) },
 
   { path: 'sign-in', loadComponent: () => import('./components/auth/sign-in/sign-in.component').then(m => m.SignInComponent) },
   { path: 'sign-up', loadComponent: () => import('./components/auth/sign-up/sign-up.component').then(m => m.SignUpComponent) },
